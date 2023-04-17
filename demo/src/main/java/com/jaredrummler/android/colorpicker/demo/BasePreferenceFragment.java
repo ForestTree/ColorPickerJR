94a64523c3acce25d3c014c3293b110dcdc0881d
Miroslav Michalec <michalec.miroslav@gmail.com>
2023-04-17 10:32:56 +0200

    update to Material3, Gradle 8.1 and AGP 8.0.0

--- demo/src/main/java/com/jaredrummler/android/colorpicker/demo/BasePreferenceFragment.java
+++ demo/src/main/java/com/jaredrummler/android/colorpicker/demo/BasePreferenceFragment.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
@@ -23,35 +24,41 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class BasePreferenceFragment extends PreferenceFragmentCompat {
 
-  @Override protected RecyclerView.Adapter onCreateAdapter(PreferenceScreen preferenceScreen) {
-    return new PreferenceGroupAdapter(preferenceScreen) {
-      @SuppressLint("RestrictedApi") @Override public void onBindViewHolder(PreferenceViewHolder holder, int position) {
-        super.onBindViewHolder(holder, position);
-        Preference preference = getItem(position);
-        if (preference instanceof PreferenceCategory) {
-          setZeroPaddingToLayoutChildren(holder.itemView);
-        } else {
-          View iconFrame = holder.itemView.findViewById(R.id.icon_frame);
-          if (iconFrame != null) {
-            iconFrame.setVisibility(preference.getIcon() == null ? View.GONE : View.VISIBLE);
-          }
-        }
-      }
-    };
-  }
-
-  private void setZeroPaddingToLayoutChildren(View view) {
-    if (!(view instanceof ViewGroup)) return;
-    ViewGroup viewGroup = (ViewGroup) view;
-    int childCount = viewGroup.getChildCount();
-    for (int i = 0; i < childCount; i++) {
-      setZeroPaddingToLayoutChildren(viewGroup.getChildAt(i));
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-        viewGroup.setPaddingRelative(0, viewGroup.getPaddingTop(), viewGroup.getPaddingEnd(),
-            viewGroup.getPaddingBottom());
-      } else {
-        viewGroup.setPadding(0, viewGroup.getPaddingTop(), viewGroup.getPaddingRight(), viewGroup.getPaddingBottom());
-      }
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected RecyclerView.Adapter onCreateAdapter(PreferenceScreen preferenceScreen) {
+        return new PreferenceGroupAdapter(preferenceScreen) {
+            @SuppressLint("RestrictedApi")
+            @Override
+            public void onBindViewHolder(PreferenceViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                Preference preference = getItem(position);
+                if (preference instanceof PreferenceCategory) {
+                    setZeroPaddingToLayoutChildren(holder.itemView);
+                } else {
+                    View iconFrame = holder.itemView.findViewById(R.id.icon_frame);
+                    if (iconFrame != null) {
+                        iconFrame.setVisibility(preference.getIcon() == null ? View.GONE : View.VISIBLE);
+                    }
+                }
+            }
+        };
     }
-  }
+
+    private void setZeroPaddingToLayoutChildren(View view) {
+      if (!(view instanceof ViewGroup)) {
+        return;
+      }
+        ViewGroup viewGroup = (ViewGroup) view;
+        int childCount = viewGroup.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            setZeroPaddingToLayoutChildren(viewGroup.getChildAt(i));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                viewGroup.setPaddingRelative(0, viewGroup.getPaddingTop(), viewGroup.getPaddingEnd(),
+                        viewGroup.getPaddingBottom());
+            } else {
+                viewGroup.setPadding(0, viewGroup.getPaddingTop(), viewGroup.getPaddingRight(), viewGroup.getPaddingBottom());
+            }
+        }
+    }
 }
